dbc4b6831d1fde90bcc5daf4420f4089e748ce99
Oleksandr Mostovyi <om@ganshina.club>
2021-05-01 20:49:10 +0300

    started

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'NgRu-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'ng-ru';
+}
